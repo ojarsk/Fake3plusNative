cbf64c640e5a1fcc00171201af0fee620929e6ad
ojarsk <98034234+ojarsk@users.noreply.github.com>
2026-04-20 11:47:55 +0000

    hitbox

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 ﻿const card = document.getElementById('card');
 const inner = document.getElementById('inner');
 
-card.addEventListener('click', () => {
+const hitbox = document.getElementById('hitbox');
+
+function flip() {
   inner.classList.toggle('flipped');
-});
+}
+
+card.addEventListener('click', flip);
+hitbox.addEventListener('click', flip);
